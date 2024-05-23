423996942029168e0364860659c4e2b6ab29f589
sebvu <santosjester738@gmail.com>
2024-05-23 01:34:34 -0500

    Add new obsidian substitutions

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -30,6 +30,78 @@ return {
 				folder = "templates",
 				date_format = "%Y-%m-%d-%a",
 				time_format = "%H:%M",
+				substitutions = {
+					capitalize_title = function()
+						-- Get the file name of the current buffer
+						local filename = vim.fn.expand("%:t:r")
+						-- Split the filename by hyphens
+						local words = {}
+						for word in string.gmatch(filename, "[^-]+") do
+							table.insert(words, word)
+						end
+						-- Capitalize each word
+						for i, word in ipairs(words) do
+							words[i] = word:sub(1, 1):upper() .. word:sub(2):lower()
+						end
+						-- Combine words to form the title
+						return table.concat(words, " ")
+					end,
+					capitalize_directory = function()
+						-- Get the directory name of the current buffer
+						local dirname = vim.fn.expand("%:p:h:t")
+						-- Split the directory name by hyphens or spaces
+						local words = {}
+						for word in string.gmatch(dirname, "[^-]+") do
+							table.insert(words, word)
+						end
+						-- Capitalize each word
+						for i, word in ipairs(words) do
+							words[i] = word:sub(1, 1):upper() .. word:sub(2):lower()
+						end
+						-- Combine words to form the title
+						return table.concat(words, " ")
+					end,
+					directory = function()
+						local dirname = vim.fn.expand("%:p:h:t")
+						local words = {}
+						for word in string.gmatch(dirname, "[^-]+") do
+							table.insert(words, word)
+						end
+						return table.concat(words, " ")
+					end,
+					professor = function()
+						-- Get the directory of the current buffer
+						local dirname = vim.fn.expand("%:p:h")
+
+						-- Iterate over files in the directory
+						for _, file in ipairs(vim.fn.readdir(dirname)) do
+							-- Check if the file matches the PROF- pattern
+							local professor_name = string.match(file, "^PROF%-(.+)$")
+							if professor_name then
+								-- Extract professor name from the matched pattern
+								return professor_name
+							end
+						end
+						-- Return nil if no matching file is found
+						return nil
+					end,
+					office_hours = function()
+						-- Get the directory of the current buffer
+						local dirname = vim.fn.expand("%:p:h")
+
+						-- Iterate over files in the directory
+						for _, file in ipairs(vim.fn.readdir(dirname)) do
+							-- Check if the file matches the PROF- pattern
+							local office_hours = string.match(file, "^OHRS%-(.+)$")
+							if office_hours then
+								-- Extract professor name from the matched pattern
+								return office_hours
+							end
+						end
+						-- Return nil if no matching file is found
+						return nil
+					end,
+				},
 				-- A map for custom variables, the key should be the varible and the value a function
 			},
 			workspaces = {
