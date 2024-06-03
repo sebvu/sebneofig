6e04a7dfe491b969c59b07dd28c893a1907715af
sebvu <santosjester738@gmail.com>
2024-06-02 21:29:42 -0500

    Update default options for colorizer.lua

--- lua/plugins/qol-plugins.lua
+++ lua/plugins/qol-plugins.lua
@@ -85,7 +85,7 @@ return {
 				user_default_options = {
 					RGB = true, -- #RGB hex codes
 					RRGGBB = true, -- #RRGGBB hex codes
-					names = true, -- "Name" codes like Blue or blue
+					names = false, -- "Name" codes like Blue or blue
 					RRGGBBAA = false, -- #RRGGBBAA hex codes
 					AARRGGBB = false, -- 0xAARRGGBB hex codes
 					rgb_fn = true, -- CSS rgb() and rgba() functions
